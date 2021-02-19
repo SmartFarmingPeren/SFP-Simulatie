937290e1702819faec9e4a1347c1cb230acb4eea
Roy_Wetterhahn <royw02@gmail.com>
2021-02-19 10:55:12 +0100

    Delete everything that is not simulation

--- SpaceColonizationSim2D/Parts/leaf.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import random as rnd
-
-
-class Leaf():
-    def __init__(self):
-        self.pos = np.array([[int(rnd.random() * 250)+50],
-                             [int(rnd.random() * 200)+50]])
-        self.reached = False
