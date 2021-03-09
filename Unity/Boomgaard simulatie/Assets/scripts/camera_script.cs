a6de0cec339d054bc643d63ed1592353cd17ec2e
Tom Lanphen <tomlanphen@gmail.com>
2021-03-09 13:00:14 +0100

    added movable camera's

---
+++ Unity/Boomgaard simulatie/Assets/scripts/camera_script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_script : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public float speed = 8.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate camera
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedH * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        // Move camera
+        CharacterController controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Unity/Boomgaard simulatie/Assets/scripts/kar_camera_script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kar_camera_script : MonoBehaviour
+{
+    public float speed = 8.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move camera
+        CharacterController controller = GetComponent<CharacterController>();
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
