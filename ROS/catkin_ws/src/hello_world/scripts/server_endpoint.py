9241833102e1206f87d79a24e2db854af28829aa
Imre Korf <I.korf@outlook.com>
2021-04-12 15:43:56 +0200

    HelloWorld package made with ROS machine as pub

---
+++ ROS/catkin_ws/src/hello_world/scripts/server_endpoint.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from ros_tcp_endpoint import TcpServer, RosPublisher, RosSubscriber, RosService
+from hello_world.msg import Text
+
+def main():
+    ros_node_name = rospy.get_param("/TCP_NODE_NAME", 'TCPServer')
+    buffer_size = rospy.get_param("/TCP_BUFFER_SIZE", 1024)
+    connections = rospy.get_param("/TCP_CONNECTIONS", 10)
+    tcp_server = TcpServer(ros_node_name, buffer_size, connections)
+    rospy.init_node(ros_node_name, anonymous=True)
+    
+    tcp_server.start({
+        'String': RosSubscriber('HelloWorld', Text, tcp_server)
+    })
+    
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
