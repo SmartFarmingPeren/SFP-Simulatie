ae7306a5ea3ddabc296bb69b65830329604dbc94
Tom Lanphen <tomlanphen@gmail.com>
2021-04-13 13:33:42 +0200

    publish joints Unity side

--- Unity/Boomgaard simulatie/Assets/scripts/camera_script.cs
+++ Unity/Boomgaard simulatie/Assets/scripts/camera_script.cs
@@ -9,10 +9,6 @@ public class camera_script : MonoBehaviour
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
-
-    public float speed = 8.0F;
-    private Vector3 moveDirection = Vector3.zero;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,18 +18,10 @@ public class camera_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Rotate camera
+		
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedH * Input.GetAxis("Mouse Y");
 
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-
-        // Move camera
-        CharacterController controller = GetComponent<CharacterController>();
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        moveDirection = transform.TransformDirection(moveDirection);
-        moveDirection *= speed;
-
-        controller.Move(moveDirection * Time.deltaTime);
     }
 }
